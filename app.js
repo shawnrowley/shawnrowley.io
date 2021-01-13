2a0f09e7968d5c595922926588d923d455dae29b
shawnrowley <shawn.a.rowley@gmail.com>
2021-01-12 22:55:20 -0500

    Add Heroku deployment port modification and Procfile

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get("/home", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is up and running on port 3000");
 });

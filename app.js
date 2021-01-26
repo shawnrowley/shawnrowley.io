89056c91a137674da8d45ffb407786f0d5b55504
shawnrowley <shawn.a.rowley@gmail.com>
2021-01-25 23:54:59 -0500

    Change google callback to .env variable | Refactor projects display

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/home",
+    callbackURL: process.env.GOOGLE_CALLBACK,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
